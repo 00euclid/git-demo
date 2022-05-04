842bdb6027cd87b589bfcd61f6fa96a2f8da9c3d
00euclid <felixsakhya666@gmail.com>
2022-05-04 12:13:35 +0545

    Added status box behavior to greet function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-document.getElementsByClassName("greeter")[0].addEventListener("click", () => alert("hello"))
+const greet = () => {
+    const response = confirm("Are you ready to enter?");
+
+    let status;
+
+    if (response) {
+        status = document.querySelector(".status.success");
+    } else {
+        status = document.querySelector(".status.failure");
+    }
+
+    status.classList.add("visible");
+}
+
+document.getElementsByClassName("greeter")[0].addEventListener("click", greet);
